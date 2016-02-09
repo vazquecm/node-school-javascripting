7f3782baed5978d90f9366744a4d11f28f230ce9
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-09 16:39:15 -0600

    verified revising strings

---
+++ javascripting/revising-strings.js
@@ -0,0 +1,10 @@
+// ## REVISING STRINGS (Exercise 5 of 19)
+
+//   You will often need to change the contents of a string.
+
+//   Strings have built-in functionality that allow you to inspect and
+//   manipulate their contents.
+
+var pizza = 'pizza is alright';
+	pizza = pizza.replace('alright', 'wonderful');
+	console.log(pizza);
