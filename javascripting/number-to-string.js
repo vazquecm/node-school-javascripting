db4334111335d87482562b1d0305bb988a92aed8
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-09 17:00:26 -0600

    verified number to string

---
+++ javascripting/number-to-string.js
@@ -0,0 +1,17 @@
+// ## NUMBER TO STRING (Exercise 8 of 19)
+
+// ## The challenge:
+
+//   Create a file named number-to-string.js.
+
+//   In that file define a variable named n that references the number 128;
+
+//   Call the .toString() method on the n variable.
+
+//   Use console.log() to print the results of the .toString() method to the
+//   terminal.
+
+
+var n = 128;
+	n.toString();
+	console.log(n);
