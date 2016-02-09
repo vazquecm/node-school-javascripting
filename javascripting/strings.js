a11569e7114c019a431141d092026f0edacbfeca
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-09 11:05:57 -0600

    verified strings

---
+++ javascripting/strings.js
@@ -0,0 +1,9 @@
+// ## STRINGS (Exercise 3 of 19)
+
+
+// when verifying the variable in the console.log don't use quotes as it's not needed since the variable is already declared above.
+
+
+var someString = 'this is a string';
+	console.log(someString);
+
