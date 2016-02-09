02418313bfb377315cf38532967b5206c15a47f7
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-09 16:50:50 -0600

    verified rounding numbers

---
+++ javascripting/rounding-numbers.js
@@ -0,0 +1,11 @@
+// ## ROUNDING NUMBERS (Exercise 7 of 19)
+
+//   We can do basic math using familiar operators like +, -, *, /, and %.
+
+//   For more complex math, we can use the Math object.
+
+//   In this challenge we'll use the Math object to round numbers.
+
+var roundUp = 1.5;
+var rounded = Math.round(roundUp);
+  console.log(rounded);
