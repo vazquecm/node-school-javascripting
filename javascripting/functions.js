a5f7114e611ff68ca9922f4e92235319e11dadb8
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-13 19:54:59 -0600

    verifying functions

---
+++ javascripting/functions.js
@@ -0,0 +1,37 @@
+// ## FUNCTIONS (Exercise 17 of 19)
+
+//   A function is a block of code that takes input, processes that input, and
+//   then produces output.
+
+//   Here is an example:
+
+//      function example (x) {
+//        return x * 2;
+//      }
+
+//   We can call that function like this to get the number 10:
+
+//      example(5)
+
+//   The above example assumes that the example function will take a number as
+//   an argument –– as input –– and will return that number multiplied by 2.
+
+//  ## The challenge:
+
+//   In that file, define a function named eat that takes an argument named
+//   food that is expected to be a string.
+
+//   Inside the function return the food argument like this:
+
+//      return food + ' tasted really good.';
+
+//   Inside of the parentheses of console.log(), call the eat() function with
+//   the string bananas as the argument.
+
+  function eat (food) {
+    return food + ' tasted really good.';
+  }
+     console.log(eat('bananas'));
+
+
+
