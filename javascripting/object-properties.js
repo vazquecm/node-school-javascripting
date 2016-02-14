8597d852556bececae7b79aecac03db6f175f216
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-13 19:08:01 -0600

    verifying object properties

---
+++ javascripting/object-properties.js
@@ -0,0 +1,49 @@
+// ## OBJECT PROPERTIES (Exercise 16 of 19)
+
+//   You can access and manipulate object properties –– the keys and values
+//   that an object contains –– using a method very similar to arrays.
+
+//   Here's an example using square brackets:
+
+//      var example = {
+//        pizza: 'yummy'
+//      };
+
+//      console.log(example['pizza']);
+
+//   The above code will print the string 'yummy' to the terminal.
+
+//   Alternately, you can use dot notation to get identical results:
+
+//      example.pizza;
+
+//      example['pizza'];
+
+//   The two lines of code above will both return yummy.
+
+// ## The challenge:
+
+//   Create a file named object-properties.js.
+
+//   In that file, define a variable named food like this:
+
+//      var food = {
+//        types: 'only pizza'
+//      };
+
+
+var food = {
+  types: 'only pizza'
+};
+
+
+/// either of these console.logs will suffice.
+  console.log(food['types']);
+  // console.log(food.types);
+
+
+
+
+
+
+
